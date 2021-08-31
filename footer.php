ce7c31b23efbba7be563d9d89bebba6562426aa2
MnoZombie956 <34872596+MnoZombie956@users.noreply.github.com>
2021-08-31 15:10:10 -0300

    base complete

---
+++ footer.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="src/style.css">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <footer>
+      <div class="divFooter">
+        <h6 style="color:blue;text-align:center;">Versão 1.0</h6>      
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <link rel = "icon" href = "logoredonda.png" type = "image/x-icon">
+  </head>
+  <body>
+    <header>
+      <div class="divHeader">
+        <img id="imglogo" src="logo.png" alt="logo">
+        <h1 id="nomeSistema">Sistema para Refeitório Estudantil</h1>
+        <h6 style="color:blue;">By Marco, Víni , Caio, Limão.<br>Prof. Evandrino | LBDI | CEFET-MG</h6>
+      </div>
+    </header>
+    <footer>
+      <div class="divFooter">
+        <h6 style="color:blue;text-align:center;">Versão 1.0</h6>      
+      </div>
+    </footer>
+  </body>
+</html>
